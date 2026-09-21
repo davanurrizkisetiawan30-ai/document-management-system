c74b19c8b6afaec856b545a21dc93ab2631c8d93
Dava <dava@gmail.com>
2026-09-21 10:48:27 +0700

    add user search

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,8 +1,18 @@
-import { users } from "../logic/userLogic";
+import { users, useUserSearch } from "../logic/userLogic";
 
 function User() {
+
+    const { search, setSearch } = useUserSearch();
+    const filteredUsers = users.filter((user) => 
+        user.name.toLowerCase().includes(search.toLowerCase()) ||
+        user.username.toLowerCase().includes(search.toLowerCase()) 
+    );
+
     return (
         <div className="activity">
+            <div className="document-action">
+                <input type="text" placeholder="Search User" value={search} onChange={(e) => setSearch(e.target.value)} />
+            </div>
             <table className="document-table">
                 <thead>
                     <tr>
@@ -14,7 +24,7 @@ function User() {
                     </tr>
                 </thead>
                 <tbody>
-                    {users.map((user) => (
+                    {filteredUsers.map((user) => (
                         <tr key={user.username}>
                             <td>{user.name}</td>
                             <td>{user.username}</td>
