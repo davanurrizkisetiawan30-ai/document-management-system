65eaf47398e43d9da5ed90283d66b422efe90f6e
Dava <dava@gmail.com>
2026-09-21 09:53:02 +0700

    add user management data dummy

---
+++ src/logic/userLogic.js
@@ -0,0 +1,23 @@
+export const users = [
+    {
+        name: "Dava Nur R S",
+        username: "Dava01",
+        companyAccess: ["Company A", "Company B"],
+        role: ["Admin", "Staff"],
+        status: "Active"
+    },
+    {
+        name: "Muhamad Farel P",
+        username: "Farel01",
+        companyAccess: ["Company B"],
+        role: ["Staff"],
+        status: "Active"
+    },
+    {
+        name: "Cathrine",
+        username: "Cath01",
+        companyAccess: ["Company B"],
+        role: ["Viewer"],
+        status: "Inactive"
+    }
+];
