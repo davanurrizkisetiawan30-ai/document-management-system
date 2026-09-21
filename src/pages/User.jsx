1391274ef905bda3acc5c53d1af4fb32a29915c3
Dava <dava@gmail.com>
2026-09-21 10:39:16 +0700

    add user management table

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,5 +1,32 @@
+import { users } from "../logic/userLogic";
+
 function User() {
-    return <h1>User</h1>;
+    return (
+        <div className="activity">
+            <table className="document-table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Username</th>
+                        <th>Company Access</th>
+                        <th>Role</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user) => (
+                        <tr key={user.username}>
+                            <td>{user.name}</td>
+                            <td>{user.username}</td>
+                            <td>{user.companyAccess.join(", ")}</td>
+                            <td>{user.role.join(", ")}</td>
+                            <td>{user.status}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default User;
